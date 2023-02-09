d38355f0f9b8753e2665c78f72f799a2fa24ab81
Jonas Blome <jonas.blome@gmx.de>
2023-02-10 00:10:49 +0100

    Added gain mapping to decibel scale

--- Chorus Delay/Source/BlomeGain.cpp
+++ Chorus Delay/Source/BlomeGain.cpp
@@ -10,6 +10,8 @@
 
 #include "BlomeGain.h"
 
+#include "JuceHeader.h"
+
 BlomeGain::BlomeGain()
 {
     
@@ -24,7 +26,11 @@ void BlomeGain::process(float* inAudio,
              float* outAudio,
              int inNumSamplesToRender)
 {
+    // Mapping gain value to decibel scale
+    float gainMapped = juce::jmap<float>(inGain, 0.0f, 1.0f, -24.0f, 24.0f);
+    gainMapped = juce::Decibels::decibelsToGain(gainMapped, -24.0f);
+    
     for(int i = 0; i < inNumSamplesToRender; i++) {
-        outAudio[i] = inAudio[i] * inGain;
+        outAudio[i] = inAudio[i] * gainMapped;
     }
 }
