66f55ff4d686ad303300e4db99f668cc086fdf49
Jonas Blome <jonas.blome@gmx.de>
2023-05-14 17:46:30 +0200

    Added cubic interpolation function for delay buffer reading.

--- Chorus Delay/Source/BlomeAudioHelper.h
+++ Chorus Delay/Source/BlomeAudioHelper.h
@@ -33,6 +33,10 @@ inline float blome_linear_interp(float v0, float v1, float t)
     return (1 - t) * v0 + t * v1;
 }
 
+inline float blome_cubic_interp (float v[4], float t) {
+    return v[1] + 0.5 * t*(v[2] - v[0] + t*(2.0*v[0] - 5.0*v[1] + 4.0*v[2] - v[3] + t*(3.0*(v[1] - v[2]) + v[3] - v[0])));
+}
+
 inline double tanh_clip(double x)
 {
     return x * (27 + x * x) / (27 + 9 * x * x);
--- Chorus Delay/Source/BlomeDelay.cpp
+++ Chorus Delay/Source/BlomeDelay.cpp
@@ -82,23 +82,38 @@ double BlomeDelay::getInterpolatedSample(float inDelayTimeInSamples)
         readPosition += maxBufferSize;
     }
     
-    int index_y0 = (int) readPosition - 1;
+    int index_y0 = (int) readPosition - 2;
     
     if(index_y0 < 0.0f) {
         index_y0 += maxBufferSize;
     }
     
-    int index_y1 = readPosition;
+    int index_y1 = (int) readPosition - 1;
     
-    if(index_y1 >= maxBufferSize) {
-        index_y1 -= maxBufferSize;
+    if(index_y1 < 0.0f) {
+        index_y1 += maxBufferSize;
+    }
+    
+    int index_y2 = (int) readPosition;
+    
+    if(index_y2 >= maxBufferSize) {
+        index_y2 -= maxBufferSize;
+    }
+    
+    int index_y3 = (int) readPosition + 1;
+    
+    if(index_y3 >= maxBufferSize) {
+        index_y3 -= maxBufferSize;
     }
     
     const float sample_y0 = mBuffer[index_y0];
     const float sample_y1 = mBuffer[index_y1];
+    const float sample_y2 = mBuffer[index_y2];
+    const float sample_y3 = mBuffer[index_y3];
     const float t = readPosition - (int)readPosition;
     
-    double outSample = blome_linear_interp(sample_y0, sample_y1, t);
+//    double outSample = blome_linear_interp(sample_y1, sample_y2, t);
+    double outSample = blome_cubic_interp(new float[] {sample_y0, sample_y1, sample_y2, sample_y3}, t);
     
     return outSample;
 }
